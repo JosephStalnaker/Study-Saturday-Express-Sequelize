ec62b71baf0dd4592efc07b3dc8846d56b6357d4
Joseph Stalnaker <jstalnaker@JosephSComputer.home>
2021-03-13 12:05:16 -0500

    second commit

--- db/models/test.js
+++ db/models/test.js
@@ -2,6 +2,19 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+//create test model first arg is the file name and the second is the object model
+const Test = db.define('test', {
+    subject: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    grade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    }
+});
+
+//create association test belongs to student
+Test.belongsTo(Student);
 
 module.exports = Test;
